3cc833a02bdaf8f6786ea7c1dca66eb4eb5199b8
abdulalkout <abdul.alkout@gmail.com>
2023-10-23 17:23:10 -0500

    created hamster class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// First
+// Firstgit 
+
+class Hamster {
+    constructor(owner = '', name, price){
+        this.owner = owner;
+        this.name = name;
+        this.price = price;
+    }
+
+    wheelRun(){
+        console.log('squeak squeak')
+    }
+
+    eatFood(){
+        console.log('nibble nibble')
+    }
+
+    getPrice(){
+        return this.price
+    }
+}
