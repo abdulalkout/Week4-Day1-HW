12635da49ab8743331208e8528b8f6a6256b6abb
abdulalkout <abdul.alkout@gmail.com>
2023-10-23 17:40:04 -0500

    created person class

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Firstgit 
 
 class Hamster {
-    constructor(owner = '', name, price){
+    constructor(owner = '', name){
         this.owner = owner;
         this.name = name;
-        this.price = price;
+        this.price = 15;
     }
 
     wheelRun(){
@@ -17,5 +17,60 @@ class Hamster {
 
     getPrice(){
         return this.price
+    }
+}
+
+
+class Person {
+    constructor(name, age=0, height=0, weight=0, mood, hamsters=[], bankAccount){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.mood = mood || 0;
+        this.hamsters = hamsters
+        this.bankAccount = bankAccount || 0;
+    }
+
+
+    getName(){
+        return this.name;
+    }
+
+    getAge(){
+        return this.age;
+    }
+
+    getWeight(){
+        return this.weight;
+    }
+
+    greet(){
+        console.log(`This is ${this.name}`);
+    }
+
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+
+    exercise(){
+        this.weight--;
+    }
+
+    ageUp(){
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount+= 10;
+    }
+
+    buyHamster(hamusterName){
+        let newHamuster = new Hamster(this.name, hamusterName)
+        this.hamsters.push(newHamuster);
+        this.mood+= 10;
+        const hamsterPrice = newHamuster.getPrice();
+        this.bankAccount = this.bankAccount - hamsterPrice;
     }
 }
